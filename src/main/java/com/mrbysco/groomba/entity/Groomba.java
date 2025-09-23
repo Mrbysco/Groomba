a15a42378a3b8cc55a5158df8b48daa731dcaaf7
Mrbysco <Mrbysco@users.noreply.github.com>
2025-09-23 21:02:44 +0200

    Nullable

--- src/main/java/com/mrbysco/groomba/entity/Groomba.java
+++ src/main/java/com/mrbysco/groomba/entity/Groomba.java
@@ -24,8 +24,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
